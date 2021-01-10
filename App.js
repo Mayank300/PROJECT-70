2afaaad86ae63eae3785a03f72ac47f821be0bb8
Mayank_Ratre <mratre300@gmail.com>
2021-01-10 20:57:03 +0530

    ADD SCREENS

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import BookTran from './screens/BookTran'
+import SearchScreen from './screens/SearchScreen'
+import { render } from 'react-dom';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const TabNavigator = createBottomTabNavigator({
+  Transation:{screen:BookTran},
+  Search:{screen:SearchScreen}
+});
+
+const AppContainer = createAppContainer(TabNavigator);
+
+
+export default class App extends React.Component {
+  render(){
+    return (
+      <AppContainer />
+    )
+  }
 }
 
 const styles = StyleSheet.create({
---
+++ screens/BookTran.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {View, Text,StyleSheet} from 'react-native'
+import { render } from 'react-dom';
+
+export default class BookTran extends React.Component{
+    render(){
+        return(
+            <View style={styles.textStyle}>
+                <Text>HELLO WORLD</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+  textStyle:{
+    textAlign:'center',
+    textSize:20,
+    justifyContent:'center',
+  }
+})
