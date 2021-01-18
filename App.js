8dae8f44043b6cdad1497188b5e41a3c900c268e
Mayank <rameshratre@Rameshs-MacBook-Air.local>
2021-01-18 20:15:40 +0530

    correct code

--- App.js
+++ App.js
@@ -2,37 +2,38 @@ import React from 'react';
 import { StyleSheet, Image } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
-import WriteStory from './screens/WriteStory'
-import ReadStory from './screens/ReadStory'
 
-const image1 = { uri: "https://wallpapercave.com/wp/wp2297884.jpg" };
-
-const TabNavigator = createBottomTabNavigator({
-  WriteStory:{screen:WriteStory},
-  ReadStory:{screen:ReadStory},
-},{
-  defaultNavigationOptions: ({navigation}) => {
-    tabBarIcon: ({ tintColor })=>{
-      const routeName = navigation.state.routeName;
-      if(routeName === 'WriteStory'){ 
-        //return (<Image style={{ width: 30, height: 30 }} source={{ uri: "https://wallpapercave.com/wp/wp2297884.jpg" }}/>)
-        return(<Image style={{ width: 30, height: 30 }}  source={require('./assets/write.png')} />)
-      } else if(routeName === 'ReadStory'){ 
-        return(<Image style={{ width: 30, height: 30 }}  source={require('./assets/read.png')} />)
-      }
-    }
-  }
-});
-
-const AppContainer = createAppContainer(TabNavigator);
+import WriteStory from './screens/WriteStory';
+import ReadStory from './screens/ReadStory';
 
 export default class App extends React.Component {
   render(){
     return (
       <AppContainer />
-    )
+    );
   }
 }
+
+const TabNavigator = createBottomTabNavigator({
+  WriteStory: {screen:WriteStory},
+  ReadStory: {screen:ReadStory},
+},
+{
+  defaultNavigationOptions: ({navigation}) => ({
+    tabBarIcon: ()=>{
+      const routeName = navigation.state.routeName;
+      if(routeName === "WriteStory"){ 
+        return(<Image style={{ width: 30, height: 30 }}  source={require('./assets/write.png')} />)
+      } else if(routeName === "ReadStory"){ 
+        return(<Image style={{ width: 30, height: 30 }}  source={require('./assets/read.png')} />)
+      }
+    }
+  })
+});
+
+const AppContainer = createAppContainer(TabNavigator);
+
+
 
 const styles = StyleSheet.create({
   container: {
